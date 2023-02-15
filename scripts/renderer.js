cfb6528b9b4921bc8f980c53c1e239d20fd00059
Liveira <ag080107@gmail.com>
2023-02-15 11:31:47 -0300

    sylveons my beloved

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -8,12 +8,14 @@ function load_posts(posts) {
     posts.forEach(post => {
         var url
         if (post.tags.general.includes("young") && post.rating != "s") {
-            url = post.file.url
+            
         } else { 
             url = post.preview.url
+            const image = fabricateElement(`<div class="hento-div"><img class="hento_post" src=${url} alt=${post.id}></img><div class="hento-info-preview"><span class="fav-count">❤️${post.fav_count}</span> <span class="score-total">⬆️${post.score.total}</span> <span class="post-rating" nonce="rAnd0m" style="color: ${post.rating == "s" ? "#58DF5A": post.rating == "e" ? "#D54010" : "#F3E211"}">${post.rating.toUpperCase()}</span></div></div>`)
+            hento_container.appendChild(image)
         }
-        const image = fabricateElement(`<div class="hento-div"><img class="hento_post" src=${url} alt=${post.id}></img><div class="hento-info-preview"><span class="fav-count">❤️${post.fav_count}</span> <span class="score-total">⬆️${post.score.total}</span> <span class="post-rating" nonce="rAnd0m" style="color: ${post.rating == "s" ? "#58DF5A": post.rating == "e" ? "#D54010" : "#F3E211"}">${post.rating.toUpperCase()}</span></div></div>`)
-        hento_container.appendChild(image);
+       
+        
     });
 }
 var searchbar = document.getElementById("tag-input")
